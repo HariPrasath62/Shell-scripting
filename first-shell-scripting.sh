d065eeb75626a92ba9f72cf652ffafb6f8cd5747
Hari Prasath <hariprasath62hp@gmail.com>
2026-03-30 14:18:13 +0000

    Shell-scripting

---
+++ first-shell-scripting.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+#create directory\
+
+mkdir first-shell-scripting
+
+#Go inside Directory 
+
+cd first-shell-scripting
+
+# create empty file
+
+touch simple-file1 simple-file2 simple-file3
+
+echo "My name is Hari" > simple-file1
+
+echo "Script executed successfully"
+
